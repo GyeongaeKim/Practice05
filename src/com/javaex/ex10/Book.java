2c98a81e78c65e453bebc942d26da24d4f0abd47
GyeongAeKim <kgae128@gmail.com>
2022-04-25 15:44:44 +0900

    2022-04-25 ex10

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -15,6 +15,8 @@ public class Book {
 		this.bookNo = bookNo;
 		this.title = title;
 		this.author = author;
+		
+		//stateCode는 생성자 호출시에 1로 세팅되도록 설정
 		this.stateCode = 1;
 	}
 	
@@ -23,7 +25,8 @@ public class Book {
     
     
     
-    //메소드
+    //메소드 - 모든 메소드는 public으로 작성
+    //bookNo, title, author에 대해서는 모든 g/s메소드 추가
     public int getBookNo() {
 		return bookNo;
 	}
@@ -42,30 +45,34 @@ public class Book {
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+	
+	
 	public int getStateCode() {
 		return stateCode;
 	}
 	public void setStateCode(int stateCode) {
 		this.stateCode = stateCode;
-	
-	
-	
-	
-    
-	
-	
-	
 	}
-
+	
+	
+	
+	
+	//rent() - 대여기능을 수행하는 메소드
 	public void rent() {
+		//Book 클래스의 stateCode를 0으로 변경하며...
 		this.stateCode=0;
+		if(stateCode==0) {
+			System.out.println("\n" + title + " 이(가) 대여 됐습니다.\n");
+		}
 	}
     
+	//print() - 책의 정보가 출력형태와 같이 출력될 수 있도록 작성;
 	public String print() {
+		
 		if(stateCode==1) {
 			return "재고있음";
 		}else {
-			return "재고없음";
+			return "대여중";
 		}
 	}
     
